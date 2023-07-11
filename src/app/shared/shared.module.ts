25cf3e40121dd0cd556ad63c0393574f529b5720
GustSilvaJR <gust.offic@gmail.com>
2023-07-10 22:38:07 -0300

    Desenvolvendo cabeçalho da pagina pos login e ajustando arquivos de estilo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   exports: [
     HeaderComponent
