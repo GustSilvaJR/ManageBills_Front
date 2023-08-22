caa47f5a55ec15bfc6c673d61f2af2d1132b2194
Gustavo <gust.offic@gmail.com>
2023-08-22 13:53:13 -0300

    Testando animações do animejs na tela de dashboard | Instalando modulo do animejs

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  
+  ngOnInit(): void {
+      
+    const path = anime.path("#demo-svg path");
+  
+    anime({
+        targets:"#teste",
+        translateX:path('x'),
+        translateY: path('y'),
+        easing: "linear",
+        duration: 3000,
+        loop: true
+    })
+  }
+  
 }
